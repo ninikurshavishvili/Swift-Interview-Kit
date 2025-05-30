a21a067a2a0f950c280bbd20bae3d30d07b2a5da
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-05-30 15:50:23 +0400

    Add file for AppState Examples

---
+++ AppState/AppState/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppState
+//
+//  Created by Nino Kurshavishvili on 30.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
