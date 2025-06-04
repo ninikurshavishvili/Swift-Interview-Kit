2f5a1b6da696a01f261b9856d38380107429e56d
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-04 21:48:24 +0400

    Add AppState LifeCycles

--- AppState/AppState/ViewController.swift
+++ AppState/AppState/ViewController.swift
@@ -11,9 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad triggered")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear triggered")
+    }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear triggered")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear triggered")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear triggered")
+    }
+
+    deinit {
+        print("ViewController is being deinitialized")
+    }
+    
 }
 
