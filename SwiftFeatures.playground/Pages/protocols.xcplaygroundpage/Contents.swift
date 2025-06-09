eefb8e2a9843aaab5f9873784a23b07a2d9c0810
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-09 08:54:32 +0400

    Property Requirements

--- SwiftFeatures.playground/Pages/protocols.xcplaygroundpage/Contents.swift
+++ SwiftFeatures.playground/Pages/protocols.xcplaygroundpage/Contents.swift
@@ -5,7 +5,16 @@ import Foundation
 // Protocols
 
 /*
- Protocol - წარმოადგენს მეთოდების, property-ების, და მოთხოვნების Blueprint-ს. მარტივად რომ ვთქვათ, პროტოკოლი არის გაწერილი მოთხოვნა, რომელიც უნდა დააწმაყოფილოს იმ კლასსმა, სტრუქტურამ, enumeration, ტიპემბა რომელიც მიიღებს ამ პროტოკოლს.
+ Protocol - წარმოადგენს მეთოდების, property-ების, და მოთხოვნების Blueprint-ს, მაგალითად როგორც კონტრაქტი ->  მარტივად რომ ვთქვათ, პროტოკოლი არის გაწერილი მოთხოვნები, რომლებიც უნდა დააწმაყოფილოს იმ კლასსმა, სტრუქტურამ, enumeration, ტიპემბა რომელიც მიიღებს ამ პროტოკოლს.
+ 
+ მაგალითად პროტოკოლებში შეჰიძლია გაწერო ასეთი ლოგილა ->
+
+ "შენ შეგიძლია გქონდეს property - name მაგრამ ის აუცილებლად უნდა იყოს String."
+ protocol SomeProtocol {
+     var name: String { get }       // ⚠️Must be readable (gettable)
+     var age: Int { get set }       // ⚠️Must be readable AND writable
+ }
+
  პროტოკოლების საუკეთესო ნაწილი ის არის რომ შეგიძლია მისი გაფართოვება. ერთი პროტოკოლის შექმნის შემდეგ რომელიც გარჯვეულ მოთხოვნებს შეიცავს, შეგილია გააკეთო მისი გაფართოება და დაამატო სხვა მოთხოვნა ან ფუნქცია. ამით გექნება მეტი მოქნილობა.
  */
 
@@ -19,4 +28,31 @@ struct SomeStructure: SomeProtocol {
 }
 // თუ რამდენიმე პროტოკოლია მათი მძიმით გამოყოფა ხდება მაგალითად :
 //struct SomeStructure: FirstProtocol, AnotherProtoco {}
+
+//Property Requirements
+/*
+ get → ნიშნავს რომ, თქვენ უნდა შეძლოთ თვისების(requirements) წაკითხვა
+
+ get set→ თქვენ უნდა შეძლოთ თვისების წაკითხვა და შეცვლა
+
+ */
+
+//✅ პროტოკოლი ერთადერთი get მოთხოვნით:
+protocol ReadOnlyProtocol {
+    var id: String { get } // Only needs to be readable
+}
+
+struct User: ReadOnlyProtocol {
+    let id: String = "12345"
+}
+
+//✅ პროტოკოლი get setმოთხოვნით:
+protocol EditableProtocol {
+    var score: Int { get set } // უნდა იყოს readable(gettable) AND writable(settable)
+}
+
+struct Player: EditableProtocol {
+    var score: Int = 0 //  იმუშავებს მხოლოდ Var-ის შემთხვევაში
+}
+
 
