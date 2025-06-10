6366be055ec5c70540666233b12ce7f12fed7d55
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-10 13:46:20 +0400

    Optionals bindings

--- SwiftFeatures.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ SwiftFeatures.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -34,5 +34,27 @@ if let myOptional {
 swift-ში optional ცვლადები წარმოადგენს მნიშვნელობის არსებობის ან არარსებობის შესძლებლობას.
  თუმცა, იმისთვის რომ optional ცვლადში შენახული მნიშვნელობა გამოიყენო, საჭიროა მისი unwrapping. optional binding - არის უსაფრთხო გზა რომ გააკეთო optional-ის unwrapping.
  
- პატარა მაგალითი
+ პატარა მაგალითი თუ როგორ აკეთებს optional binding-ი safe unwraping-ს.
  */
+
+let possibleString: String? = "hello optional"
+if let realString = possibleString {
+    print("\(realString)!")
+} else {
+    print("nil")
+}
+
+ /*
+  ამ შემთხვევაში, ჩვენ unwapped მნიშვნელობას (არსებობის შემთხვევაში) ვინახავთ ახალ ცვლადში realString-ში, რომელიც უკვე არის კონსტანტა (let), და არა optional-ი.
+  თუ optional მნიშვნელობა nil იქნებოდა, მაშინ პიველი პირობა არ შესრულდებოდა, მხოლოდ მეორე else ბლოკი შესრულდება.
+  
+  
+  აღსაღნიშნია რომ, მიღებული პრაქტიკაა, რომ optional-ებს unwraping-ის დროს დაარქვა იგივე სახელი რაც optional-ს. მაგალითად აქ realString-ის ნაცვლად უნდა იყოს:
+  if let  possibleString {
+  print("\(possibleString)!")
+}
+  */
+
+// Unwrap Multiple Optionals at Once
+
+
