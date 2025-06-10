cb798176d096b4e67df275cb3ad193baa0005946
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-10 19:35:34 +0400

    Optionals done

---
+++ SwiftFeatures.playground/Pages/HOF.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+// https://www.kodeco.com/books/swift-cookbook/v1.0/chapters/8-use-optional-map-flatmap
