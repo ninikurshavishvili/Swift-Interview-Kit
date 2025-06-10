488b985de381ff9fb1f5eee7cc19bad995516df6
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-10 13:36:44 +0400

    Optionals Start

--- SwiftFeatures.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ SwiftFeatures.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -5,5 +5,34 @@ import Foundation
 //Optionals
 
 /*
+ Optional-ები Swift-ის ძლიერი და სუპერ გამოსადეგი feature-ია. ის საშუალებას გვაძლევს რომ კოდში ცარიელი ან დაკარგული მნიშვნელობები დავამუშავოთ. ანუ Optional ცვლადს შეიძლება ქონდეს value ან შეიძლება იყოს nil.
  
+აქ არის პატარა მაგალითი, თუ როგორ უნდა გამოვაცხადოთ და ვემუშაოთ optional-ებს
+ */
+
+var myOptional: Int?
+
+myOptional = 3
+
+if let myOptional {
+    print(myOptional)
+} else {
+    print("nil")
+}
+
+myOptional = nil
+
+if let myOptional {
+    print(myOptional)
+} else {
+    print("nil")
+}
+
+//Optional Binding in Swift
+
+/*
+swift-ში optional ცვლადები წარმოადგენს მნიშვნელობის არსებობის ან არარსებობის შესძლებლობას.
+ თუმცა, იმისთვის რომ optional ცვლადში შენახული მნიშვნელობა გამოიყენო, საჭიროა მისი unwrapping. optional binding - არის უსაფრთხო გზა რომ გააკეთო optional-ის unwrapping.
+ 
+ პატარა მაგალითი
  */
