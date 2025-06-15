6d60b7a7b4a6467bc62756e542563b3ea26a50bf
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-15 23:33:09 +0400

    Value Types & Reference Types

---
+++ SwiftFeatures.playground/Pages/ReferenceVSValueTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+//Value Types & Reference Types
+
+// ზოგადად Swift-ში, მონაცმეთა ორი ტიპი არსებობს value და reference type.
+
+/*
+ Value type არის მონაცემთა ისეთი ტიპი რომლის მნიშვნელობაც ინახება პირდაპირ მეხსიერებაში.
+ 
+ Referece type არის მონაცემთა ისეთი ტიპი, რომლის მნიშვნელობაც ინახება, როგორც Reference-ი მეხსიერებაში, იმ ადგილას სადაც მისი Actual value არის შენახული.
+ */
+
+//Reference Type-ები არიან Class-ები და Closure-ები.
+
