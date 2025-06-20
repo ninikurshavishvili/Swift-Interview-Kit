fff54d2726dad7753dba3824727338549af12a45
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-20 16:48:31 +0400

    Start of Design Patterns in Swift

---
+++ DesignPatterns/DesignPatterns/DesignPatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsApp.swift
+//  DesignPatterns
+//
+//  Created by Nino Kurshavishvili on 20.06.25.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
