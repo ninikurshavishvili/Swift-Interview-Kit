0dbbdb8778f758749873dd4f0bc420ffd2f17928
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-21 12:42:27 +0400

    Start of Design Patterns MVVM

--- DesignPatterns/DesignPatterns/MVVM/WeatherView.swift
+++ DesignPatterns/DesignPatterns/MVVM/WeatherView.swift
@@ -5,3 +5,25 @@
 //  Created by Nino Kurshavishvili on 21.06.25.
 //
 
+import SwiftUI
+
+struct WeatherView: View {
+    @StateObject private var viewModel = WeatherViewModel()
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Temperature: \(viewModel.weather)")
+                .font(.title)
+            Text("Condition: \(viewModel.weather.condition)")
+                .font(.headline)
+            
+            Button("Refresh Weather") {
+                viewModel.updateWeather()
+            }
+            .padding()
+            .background(Color.primary)
+            .cornerRadius(8)
+        }
+        .padding(20)
+    }
+}
