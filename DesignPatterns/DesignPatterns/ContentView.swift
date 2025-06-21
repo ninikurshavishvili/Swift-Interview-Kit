5e7a9d16aabecf7278b17ba5fec15042f4507725
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-21 09:53:09 +0400

    Start of Design Patterns

--- DesignPatterns/DesignPatterns/ContentView.swift
+++ DesignPatterns/DesignPatterns/ContentView.swift
@@ -9,15 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        UserViewControllerWrapper()
+            .edgesIgnoringSafeArea(.all) // optional if you want it full-screen
     }
 }
+
 
 #Preview {
     ContentView()
