9795c94061b8fb0e10eff3379d0b4dc10ddbbeb3
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-27 22:15:11 +0400

    Add ARC Project Files

---
+++ ARCInSwift/ARCInSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ARCInSwift
+//
+//  Created by Nino Kurshavishvili on 27.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
