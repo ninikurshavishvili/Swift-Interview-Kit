d80bd98b210f9852966cf977d0705240d7f68d4c
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-29 00:06:16 +0400

    Add Unowned Reference Examples

--- ARCInSwift/ARCInSwift/UnownedReference.swift
+++ ARCInSwift/ARCInSwift/UnownedReference.swift
@@ -17,3 +17,36 @@
 4. ⚠️ თუ ობიექტის დეალოკაცია მოხდა, და მასზე წვდომას ვცდით unowned რეფერენსით -> იწვევს პროგრამის დაქრაშვას
  [⚠️ If the object is deallocated and accessed via unowned → crash!]
  */
+
+class Customer {
+    let name: String
+    var card: CreditCard?
+    
+    init(name: String) {
+        self.name = name
+        print("💳 Customer \(name) initialized")
+    }
+    deinit {
+        print("💳 Customer \(name) deinitialized")
+    }
+}
+
+class CreditCard {
+    let number: Int
+    unowned let customer: Customer // <- unowned reference
+
+    init(number: Int, customer: Customer) {
+        self.number = number
+        self.customer = customer
+        print("💳 Card \(number) initialized for \(customer.name)")
+    }
+    deinit {
+        print("💳 Card \(number) deinitialized")
+    }
+}
+
+func runUnownedReferenceExample() {
+    var c: Customer? = Customer(name: "Nini")
+    c?.card = CreditCard(number: 1234567890, customer: c!)
+    c = nil // 🔥 Card AND customer deinitialized
+}
