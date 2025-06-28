c1763f59e81d5160bd0d18b1a2223cea0823c584
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-28 23:51:47 +0400

    Add strong ref general info and example

--- ARCInSwift/ARCInSwift/StrongReference.swift
+++ ARCInSwift/ARCInSwift/StrongReference.swift
@@ -7,16 +7,48 @@
 
 // Strong Reference
 /*
- მოდი განვიხილოთ შემდეგი მაგალითი.
+რას გულისხმობს Strong Reference.
+ 1. კლასებში, ყველა pproperty-ს default-ად აქვს ძლიერი ბმა.
+ 2. ზრდის ობიექტის reference count-ის რაოდენობას.
+ 3. ⚠️ If two objects hold strong references to each other → ⚠️ retain cycle.
+ (თუ ორ ობიქტს უჭირავს ძლიერი ბმით ერთმანეთი -> ეს იწვევს ⚠️ retain cycle-ს.
  
- */
 
+ 
+*/
 
-/*
- var kelvin: Person?
- var iphone: Gadget?
+class Teacher {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("👨‍🏫 Teacher \(name) initialized")
+    }
+    var student: Student?
+    deinit {
+        print("👨‍🏫 Teacher \(name) deinitialized")
+    }
+}
 
+class Student {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("👧 Student \(name) initialized")
+    }
+    var teacher: Teacher? // <- STRONG reference!
+    deinit {
+        print("👧 Student \(name) deinitialized")
+    }
+}
 
- kelvin = Person(name: "Kelvin")
- iphone = Gadget(model: "iPhone 8 Plus")
- */
+func runStrongReferenceExample() {
+    var t: Teacher? = Teacher(name: "Mr. Joe")
+    var s: Student? = Student(name: "Nini")
+    
+    t?.student = s
+    s?.teacher = t
+    
+    t = nil
+    s = nil // ❌ deinit will NOT be called because of strong retain cycle
+}
+
