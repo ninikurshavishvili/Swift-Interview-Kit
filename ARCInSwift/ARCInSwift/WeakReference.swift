4d0e69300670bb6a5c1eba558e5f2bc0b158b0b2
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-28 23:58:35 +0400

    Add weak reference Example

--- ARCInSwift/ARCInSwift/WeakReference.swift
+++ ARCInSwift/ARCInSwift/WeakReference.swift
@@ -13,3 +13,38 @@
  2. ის ყოველთვის არის optional-ი, რადგან ობიექტი რომელზეც ის მიუთითებს, შეიძლება გახდეს nil-ი როცა მისი დეალოკაცია მოხდება.
  3. საუკეთესოდ გამოიყენება, როდესაც child კლასით იჭერ parent-ის რეფერენსს (ანუ სხვა სიტყვებით, როცა ჩაილდ კლასი მშობელ კლასს მიმართავს) [ Best used when child holds reference to parent.].
  */
+
+class Dog {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("🐶 Dog \(name) initialized")
+    }
+    var owner: Owner?
+    deinit {
+        print("🐶 Dog \(name) deinitialized")
+    }
+}
+
+class Owner {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("👤 Owner \(name) initialized")
+    }
+    weak var pet: Dog? // <- weak reference
+    deinit {
+        print("👤 Owner \(name) deinitialized")
+    }
+}
+
+func runWeakReferenceExample() {
+    var dog: Dog? = Dog(name: "Rex")
+    var owner: Owner? = Owner(name: "Nini")
+    
+    dog?.owner = owner
+    owner?.pet = dog
+    
+    dog = nil  // 🗑 Dog is released
+    owner = nil  // 🗑 Owner is also released (no retain cycle)
+}
