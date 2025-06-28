235d13544399278be67a09204491493537efd929
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-28 23:58:04 +0400

    Add weak reference information

--- ARCInSwift/ARCInSwift/WeakReference.swift
+++ ARCInSwift/ARCInSwift/WeakReference.swift
@@ -5,3 +5,11 @@
 //  Created by Nino Kurshavishvili on 28.06.25.
 //
 
+// Weak Reference
+/*
+ რას გულისხმობს Weak Reference
+ 
+ 1. სუსტი ბმა არასოდეს ზრდის reference count-ის რაოდენობას
+ 2. ის ყოველთვის არის optional-ი, რადგან ობიექტი რომელზეც ის მიუთითებს, შეიძლება გახდეს nil-ი როცა მისი დეალოკაცია მოხდება.
+ 3. საუკეთესოდ გამოიყენება, როდესაც child კლასით იჭერ parent-ის რეფერენსს (ანუ სხვა სიტყვებით, როცა ჩაილდ კლასი მშობელ კლასს მიმართავს) [ Best used when child holds reference to parent.].
+ */
