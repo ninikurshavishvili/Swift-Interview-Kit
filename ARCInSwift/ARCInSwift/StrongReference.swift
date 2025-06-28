03343249ff99ab32e17b6d4a033f3156c8cd8b02
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-06-28 23:31:19 +0400

    Start strong ref examples

--- ARCInSwift/ARCInSwift/StrongReference.swift
+++ ARCInSwift/ARCInSwift/StrongReference.swift
@@ -5,3 +5,18 @@
 //  Created by Nino Kurshavishvili on 27.06.25.
 //
 
+// Strong Reference
+/*
+ 
+ 
+ */
+
+
+/*
+ var kelvin: Person?
+ var iphone: Gadget?
+
+
+ kelvin = Person(name: "Kelvin")
+ iphone = Gadget(model: "iPhone 8 Plus")
+ */
