73754a4b38f058a6f9b9ae3ca3959b2a0f26b523
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-07-02 07:53:54 +0400

    Add Overlays 2 parts

--- OverlayExample/OverlayExample/ContentView.swift
+++ OverlayExample/OverlayExample/ContentView.swift
@@ -5,20 +5,8 @@
 //  Created by Nino Kurshavishvili on 02.07.25.
 //
 
-import SwiftUI
+// განვიხილოთ Overlays ორი მნიშვნელობა: Overlays (OS/Memory Concept), Overlay in SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-#Preview {
-    ContentView()
-}
+// რა არის Overlay (სისტემის ნაწილში) :
+
