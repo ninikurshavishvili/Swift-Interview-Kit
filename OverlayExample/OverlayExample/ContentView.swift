a73c89391a47e6f00f6ad62ba88fa5b0e6c4271f
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-07-02 08:10:12 +0400

    Add Overlays SwiftUI example

--- OverlayExample/OverlayExample/ContentView.swift
+++ OverlayExample/OverlayExample/ContentView.swift
@@ -9,4 +9,36 @@
 
 
 // რა არის Overlay (სისტემის ნაწილში) :
+/*
+ დაბალი დონის სისტემებში ( მაგალითად, ჩაშენებული სისტემები ან ადრეული ოპერაციული სისტემები):
+ | Overlay (გადაფარვები) არის ტექნიკა რომელიც-> გამოიყენება დიდი პროგრამის წანილებად დასაშლელად და მეხსიერებაში მხოლოდ საჭირო პროგრამის ნაწილის ჩასატვირთად.
+ | ზოგავს მემორის, მხოლოდ ამჟამად საჭირო კოდის ჩათვირთვით.
+ | საუკეთესოა პატარა RAM-ის მქონე სისტემებისთვის.
+ */
 
+
+//რა არის .overlay() Swift-ში.
+
+/*
+ overlay() - არის midifier, რომელიც ამატებს ახალ ვიუს, უკვე არსებული ვიუს ზემოდან, როგორც როგორც ახალი layer.
+ 
+ ძირითადი Use Cases:
+ Add a border
+
+ Add a label on top of an image
+
+ Add a semi-transparent mask or background
+
+ Display loading spinners or alerts
+ */
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isPressed = false
+    var body:some View {
+        VStack {
+            
+        }
+    }
+}
