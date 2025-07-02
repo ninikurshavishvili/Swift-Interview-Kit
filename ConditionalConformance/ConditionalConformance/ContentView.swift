882baf2a3af6e9f6ff5e5cd8de12e63049e550f5
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-07-02 11:02:36 +0400

    Add Conditional Conformance project files

--- ConditionalConformance/ConditionalConformance/ContentView.swift
+++ ConditionalConformance/ConditionalConformance/ContentView.swift
@@ -5,20 +5,4 @@
 //  Created by Nino Kurshavishvili on 02.07.25.
 //
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    ContentView()
-}
+// რა არის Conditional Conformance (პირობითი შესაბამისობები)
