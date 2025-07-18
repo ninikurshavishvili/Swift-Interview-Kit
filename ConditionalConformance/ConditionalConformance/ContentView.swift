35f78f51667d1df740bda6117a01393de7ec0237
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-07-18 17:04:24 +0400

    Add Conditional Conformance General info

--- ConditionalConformance/ConditionalConformance/ContentView.swift
+++ ConditionalConformance/ConditionalConformance/ContentView.swift
@@ -5,6 +5,16 @@
 //  Created by Nino Kurshavishvili on 02.07.25.
 //
 
+
 // რა არის Conditional Conformance (პირობითი შესაბამისობები)
-//some new changess..??qq
+/*
+ Swift-ში Conditional Conformance გულისხმობს, რომ Generic ტიპი შეესაბამება პროტოკოლს მხოლოდ გარკვეულ პირპბებში. და რას ნიშნავს ეს : ეს ნიშნავს რომ ტიპი შეესაბამება პროტოკოლს მხოლოდ იმ შემთხვევაში, თუ მისი Generic პარამეტრიც ასევე შეესაბამება ამ პროტოკოლს.
+ Conditional Conformance - გამოვიდა Swift 4.1 ვერსიასთან და ძირითადად გამოიყენება Array, Optional, და custom generic Struc და enum-ებთან.
+ */
+
+//მაგალითად, თუ გვაქვს რაიმე generic ტიპი და გვსურს, რომ ის შეესაბამებოდეს პროტოკოლს, მხოლოდ იმ შემთხვევაში, თუ T-ც შეესაბამება ამ პროტოკოლს. იმის ნაცვლად რომ force to always conform, უნდა დაწერო : extension SomeExtension: Someprotocol where T: Someprotocol { }
+
+import SwiftUI
+
+
 
