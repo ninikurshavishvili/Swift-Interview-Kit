320e10095d3a32c08d755f8ccf522a2b5a5366cb
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-07-19 15:07:47 +0400

    Add ButtonStyle Project folders

---
+++ ButtonStyleExample/ButtonStyleExample/ButtonStyleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStyleExampleApp.swift
+//  ButtonStyleExample
+//
+//  Created by Nino Kurshavishvili on 19.07.25.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStyleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonStyleExample/ButtonStyleExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ButtonStyleExample
+//
+//  Created by Nino Kurshavishvili on 19.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
