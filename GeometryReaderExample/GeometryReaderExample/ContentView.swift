4c5067c0a5653028dbcb87fa9ff6b75974afd738
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-09-12 22:04:56 +0400

    Add GeometryReader example Project files

---
+++ GeometryReaderExample/GeometryReaderExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeometryReaderExample
+//
+//  Created by Nino Kurshavishvili on 12.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Geometry Reader Info goes here...")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeometryReaderExample/GeometryReaderExample/GeometryReaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderExampleApp.swift
+//  GeometryReaderExample
+//
+//  Created by Nino Kurshavishvili on 12.09.25.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
